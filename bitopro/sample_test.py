df5c37cd8f95e5b7bfbbf179623e8b839445b797
Kuan <j19920816@yahoo.com.tw>
2023-07-25 22:07:57 +0800

    wrap websocket api

--- bitopro/sample_test.py
+++ bitopro/sample_test.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 import json
+from bitopro_restful_client import OrderType
 from bitopro_util import build_headers, get_current_timestamp
 from bitopro_restful_client import BitoproRestfulClient, CandlestickResolutin, OrderStatus, WithdrawProtocol
-from bitopro_websocket_client import BitoproExWebsocket, BitoproWebsocketEndpoint
+import bitopro_websocket_client as bitopro_ws
 
 account = ""
 apiKey = ""
@@ -54,15 +55,21 @@ def bitopro_restful_test():
     pair = "BTC_USDT"
     order_id = ""
 
-    # [POST] Create an order
-    create_order_response = bitopro_client.create_an_order(action="BUY", amount=0.0001, price=10500, pair=pair)
+    # [POST] Create a limit order
+    create_order_response = bitopro_client.create_an_order(action="BUY", amount=0.0001, price=10500, pair=pair, client_id=587456)
     if create_order_response is not None:
         order_id = create_order_response["orderId"]
-    print("Order created:", create_order_response)
-    
+    print("Limit order created:", create_order_response)
+
     # [GET] Get an order
     get_an_order_response = bitopro_client.get_an_order(pair, order_id)
     print("Get an order:", get_an_order_response)
+
+    # [POST] Create a market order
+    create_order_response = bitopro_client.create_an_order(action="BUY", amount=34, pair="BTC_USDT", type=OrderType.Market)
+    if create_order_response is not None:
+        order_id = create_order_response["orderId"]
+    print("Market order created:", create_order_response)
 
     # [DELETE] Cancel the order
     if create_order_response is not None:
@@ -71,7 +78,7 @@ def bitopro_restful_test():
         print("Order cancelled:", cancel_order_response)
 
     # [GET] get all orders
-    response = bitopro_client.get_all_orders(pair, status=OrderStatus.Completed)
+    response = bitopro_client.get_all_orders(pair, start_timestamp=1679299428000, status=OrderStatus.Completed)
     print("All orders:", response)
 
     # [GET] get all trades
@@ -194,32 +201,36 @@ def bitopro_restful_test():
     # [POST] Withdraw
     get_withdraw_response = bitopro_client.withdraw("USDT", WithdrawProtocol.ERC20, "", 50.5, "")
     print("GET withdraw:", get_withdraw_response)
+    
 
 def bitopro_websocket_test():
+    symbols_list = {"eth_btc": 5, "BTC_TWD": 1, "ETH_TWD": 20, "BITO_ETH": 1}
+    symbols = ["BTC_TWD", "ETH_TWD", "BITO_ETH"]
+
     # [PUBLIC] GET Order book
-    bito_websocket_order_book = BitoproExWebsocket(BitoproWebsocketEndpoint + "/v1/pub/order-books/eth_btc:1", websocket_handler)
-    bito_websocket_order_book.init_websocket()
-    bito_websocket_order_book.start()
+    bitoproWs_order_book = bitopro_ws.BitoproOrderBookWs(symbols_list, websocket_handler)
+    bitoproWs_order_book.init_websocket()
+    bitoproWs_order_book.start()
 
     # [PUBLIC] GET Ticket
-    bito_websocket_ticker = BitoproExWebsocket(BitoproWebsocketEndpoint + "/v1/pub/tickers/eth_btc", websocket_handler)
+    bito_websocket_ticker = bitopro_ws.BitoproTickerkWs(symbols, websocket_handler)
     bito_websocket_ticker.init_websocket()
     bito_websocket_ticker.start()
 
     # [PUBLIC] GET Trade
-    bito_websocket_trades = BitoproExWebsocket(BitoproWebsocketEndpoint + "/v1/pub/trades/eth_btc", websocket_handler)
+    bito_websocket_trades = bitopro_ws.BitoproTradesWs(symbols, websocket_handler)
     bito_websocket_trades.init_websocket()
     bito_websocket_trades.start()
     
     # [Private] GET active orders
-    bito_websocket_order_book = BitoproExWebsocket(BitoproWebsocketEndpoint + "/v1/pub/auth/orders", websocket_handler, account, apiKey, apiSecret)
-    bito_websocket_order_book.init_websocket()
-    bito_websocket_order_book.start()
-
+    bito_websocket_user_order = bitopro_ws.BitoproUserOrdersWs(account, apiKey, apiSecret, websocket_handler)
+    bito_websocket_user_order.init_websocket()
+    bito_websocket_user_order.start()
+    
     # [Private] GET account balance
-    bito_websocket_order_book = BitoproExWebsocket(BitoproWebsocketEndpoint + "/v1/pub/auth/account-balance", websocket_handler, account, apiKey, apiSecret)
-    bito_websocket_order_book.init_websocket()
-    bito_websocket_order_book.start()
+    bito_websocket_user_balance = bitopro_ws.BitoproUserBlanceWs(account, apiKey, apiSecret, websocket_handler)
+    bito_websocket_user_balance.init_websocket()
+    bito_websocket_user_balance.start()
 
 def websocket_handler(message:str):
     reply = json.loads(message)
@@ -235,7 +246,7 @@ def websocket_handler(message:str):
         print("TRADE: ", reply, end="\n\n")
         
 if __name__ == "__main__":
-    bitopro_restful_test()
+    # bitopro_restful_test()
     bitopro_websocket_test()
 
     
